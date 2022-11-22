e721ff0fcf725822a2a63c4c44c9484010a23c28
Dev1-dipanetech <dev1@dipanetech.com>
2022-11-22 12:55:36 +0530

    day 2

---
+++ superstar_rides/superstar_rides/doctype/ride/ride.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Surya and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Ride', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ superstar_rides/www/success.py
@@ -0,0 +1,7 @@
+import frappe
+
+no_cache = 1
+
+def get_context(context):
+    context.secret_message = "Something cool"
+    return context
